27049d104738858c87b7d68cc61cfc33f1abe4b1
HangNT169 <hangnt169@fpt.edu.vn>
2025-08-08 21:40:27 +0700

    Buổi 1: CRUD list fix cứng - Phần 1

---
+++ B1_2_CRUD_List_Fix_Cung/src/components/FormSachComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="container">
+    <!-- Form Sach Component  -->
+    <form>
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Tên</label>
+        <input type="text" class="form-control" />
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Loại</label>
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Tác giả</label>
+        <input type="text" class="form-control" />
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Giá</label>
+        <input type="text" class="form-control" />
+      </div>
+    </form>
+  </section>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ B1_2_CRUD_List_Fix_Cung/src/pages/SachPage.vue
@@ -0,0 +1,88 @@
+<template>
+  <h1>Quản lý sách</h1>
+  <FormSachComponent />
+  <div class="container">
+    <button type="button" class="btn btn-outline-success">Add</button>
+    <button type="button" class="btn btn-outline-primary">Update</button>
+  </div>
+
+  <table class="table container">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Tên</th>
+        <th scope="col">Loại</th>
+        <th scope="col">Tác giả</th>
+        <th scope="col">Giá</th>
+        <th scope="col">Hành động</th>
+      </tr>
+    </thead>
+    <tbody>
+      <template v-for="item in listSach" :key="item.id">
+        <tr>
+          <td>{{ item.id }}</td>
+          <td>{{ item.ten }}</td>
+          <td>{{ item.loai }}</td>
+          <td>{{ item.tacGia }}</td>
+          <td>{{ item.gia }}</td>
+          <td>
+            <button type="button" class="btn btn-outline-primary">Detail</button>
+            <button type="button" class="btn btn-outline-danger">Delete</button>
+          </td>
+        </tr>
+      </template>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+import FormSachComponent from '@/components/FormSachComponent.vue'
+import { ref } from 'vue'
+const listSach = ref([
+  {
+    // liet ke cac thuoc tinh cua sach
+    id: 1,
+    ten: 'Sach 1',
+    loai: 'Loai 1',
+    tacGia: 'Tac Gia 1',
+    gia: 100000,
+  },
+  {
+    id: 2,
+    ten: 'Sach 2',
+    loai: 'Loai 2',
+    tacGia: 'Tac Gia 2',
+    gia: 150000,
+  },
+  {
+    id: 3,
+    ten: 'Sach 3',
+    loai: 'Loai 1',
+    tacGia: 'Tac Gia 3',
+    gia: 80000,
+  },
+  {
+    id: 4,
+    ten: 'Sach 4',
+    loai: 'Loai 3',
+    tacGia: 'Tac Gia 4',
+    gia: 120000,
+  },
+  {
+    id: 5,
+    ten: 'Sach 5',
+    loai: 'Loai 2',
+    tacGia: 'Tac Gia 5',
+    gia: 90000,
+  },
+])
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+button {
+  margin-right: 50px;
+}
+</style>
